65957f9a9d5a18d7d2bc78ceee3d0011dcd4c994
Mohamed Zerrouki <mohamed.zerrouki@oclock.school>
2024-05-13 13:46:16 +0200

    feat: init server express

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import router from './app/routers.js';
+
+require('dotenv').config();
+
+const port = process.env.PORT || '3000';
+const app = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Server ready: http://localhost:${port}`);
+});
