17a7a5b4736b5dfcb775b1eb880af2fd0e2d9976
Mohamed Zerrouki <mohamed.zerrouki@oclock.school>
2024-05-14 12:31:40 +0200

    refactor: delete obsolete project_details_view.sql

--- db/migrations/revert/project_details_view.sql
+++
@@ -1,7 +0,0 @@
--- Revert unity_manager:project_details_view from pg
-
-BEGIN;
-
-DROP VIEW IF EXISTS project_details;
-
-COMMIT;
--- db/migrations/verify/project_details_view.sql
+++
@@ -1,9 +0,0 @@
--- Verify unity_manager:project_details_view on pg
-
-BEGIN;
-
-SELECT 1
-FROM information_schema.views
-WHERE table_name = 'project_details';
-
-ROLLBACK;
